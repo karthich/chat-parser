07b40b5ea1b91fe51fb6e4b6964795a70624bcb0
Karthic Hariharan <khariharan@rmn.com>
2015-01-31 17:43:55 -0600

    Writing simple tests for mentions

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
+import unittest
+from chat_parser import ChatParser
+
 __author__ = 'khariharan'
+
+class TestChatParser(unittest.TestCase):
+
+    def setUp(self):
+        # Nothing to do here yet
+        self.parser = ChatParser()
+
+
+    def test_mentions(self):
+        output = self.parser.parse('Hey there my friend @Roman!')
+        self.assertEqual(output, '{"mentions": ["Roman"]}')
+
+    def test_multiple_mentions(self):
+        output = self.parser.parse("It's time for standup @Chris, @Rahul and @Ray!")
+        self.assertEqual(output, '{"mentions": ["Chris", "Rahul", "Ray"]}')
 
