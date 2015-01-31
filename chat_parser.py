ff21725e056b8993868b843d6d3fea155734ed53
Karthic Hariharan <khariharan@rmn.com>
2015-01-31 17:32:56 -0600

    Stub for chat parser

---
+++ chat_parser.py
@@ -0,0 +1,10 @@
+__author__ = 'khariharan'
+
+
+class ChatParser(object):
+    # This is just a stub for now
+    """
+        message - text message to parse for mentions, emoticons, urls
+    """
+    def parse(self, message):
+        return {}
